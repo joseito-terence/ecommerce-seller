d1aa97375b359f99af23d9d3a3ec360e12c99934
Charlton-Dias <charltondias29@gmail.com>
2021-03-06 20:26:41 +0530

    Completed User Update UI

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -2,114 +2,313 @@ import React, { useState, useRef } from "react";
 import "./User.css"
 import db, { auth, storage } from '../../firebase';
 
-function UserUpdate(){
+function UserUpdate() {
+    const tenYears = () => {
+        const currYear = new Date().getFullYear();
+        const list = [];
+        for (let year = currYear; year <= currYear + 10; year++) {
+            list.push(year);
+        }
+        return list;
+    }
+
     const initialState = {
-        fname: '', 
-        lname: '', 
-        phone: '', 
-        email: '', 
-    
+        fname: '',
+        lname: '',
+        phone: '',
+        email: '',
+
         storeName: '',
         shopNo: '',
         pincode: '',
-        city: '', 
-        state: '', 
-        country: '', 
-    
-        cardHoldersName: '', 
+        city: '',
+        state: '',
+        country: '',
+
+        cardHoldersName: '',
         cardNumber: '',
         cvv: '',
-        month: '', 
+        month: '',
         year: '',
     };
 
-    const [state, setState ] = useState(initialState);
+    const [state, setState] = useState(initialState);
     const [originalInfo, setOriginalInfo] = useState({}); // stores the original user information to revert back onCancel.
     const [isDisabled, setIsDisabled] = useState(false);
     const uid = auth.currentUser.uid;
-    
+
     const handleChange = ({ target }) => {
         setState({ ...state, [target.name]: target.value });
-      }
+    }
+
     //reset and submit needs to be created
     const reset = () => {
         // reset function...
     }
-    
+
     const submit = event => {
         event.preventDefault();
 
         // logic for submit goes here...
     }
 
-    return(
+    return (
         <div className="UpdateUser">
             <form onSubmit={submit} method='post'>
-
                 <div className="container" >
                     {/* Account */}
-                    <div className="row mb-3">
+                    <div className="row mb-2 mt-3">
                         <h3>Account</h3>
-                        <hr/>
+                        <hr />
+
                         <div className="col">
-                            <label className="text-end fs-5" htmlFor="fname">First Name</label>
-                            <input type="text" name='fname' className='form-control' value={state.fname} onChange={handleChange} disabled={isDisabled} required/>
+                            <label className="text-end fs-5" htmlFor="fname">First Name </label>
+
+                            <input type="text" name='fname'
+                                className='form-control'
+                                value={state.fname}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required />
                         </div>
-                    
+
                         <div className="col">
                             <label className="text-end fs-5" htmlFor="lname">Last Name</label>
-                            <input type="text" name='lname' className='form-control' value={state.lname} onChange={handleChange} disabled={isDisabled} required/>
+
+                            <input
+                                type="text"
+                                name='lname'
+                                className='form-control'
+                                value={state.lname}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required />
                         </div>
                     </div>
-                    <div className="row mb-5">
+
+                    <div className="row mb-2">
                         <div className="col">
                             <label className="text-end fs-5" htmlFor="phone">Phone No.</label>
-                            <input type="text" name='phone' className='form-control' value={state.phone} onChange={handleChange} disabled={isDisabled} required
-                            pattern='^\d+$' title='Only Numbers allowed.' />
+
+                            <input
+                                type="text"
+                                name='phone'
+                                className='form-control'
+                                value={state.phone}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required
+                                pattern='^\d+$'
+                                title='Only Numbers allowed.' />
                         </div>
+
                         <div className="col">
                             <label className="text-end fs-5" htmlFor="fname">Email</label>
-                            <input type="text" name='email' className='form-control' value={state.email} onChange={handleChange} disabled={isDisabled} required/>
+
+                            <input
+                                type="text"
+                                name='email'
+                                className='form-control'
+                                value={state.email}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required />
                         </div>
                     </div>
+
+                    <div className="button btn btn-link mb-3"> Change Password </div>
+
 
                     {/* Business Information */}
-                    <h4>Business Info </h4>
-                    <hr />
-                    <div className="row mb-3">
-                        <div className="col"></div>
-                        <div className="col"></div>
-                    </div>
-                    <div className="row mb-3">
-                        <div className="col"></div>
-                        <div className="col"></div>
-                    </div>
-                {/* Billing Information */}
-                    <div className="row">
-                        <div className="col">
-                        <h4>Billing Info </h4>
+                    <div className="row mb-2">
+                        <h3>Business Info </h3>
                         <hr />
+
+                        <div className="col">
+                            <label className="text-end fs-5" htmlFor="storeName"> Store Name </label>
+
+                            <input type="text"
+                                name='storeName'
+                                className='form-control'
+                                value={state.storeName}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required />
+                        </div>
+
+                        <div className="col">
+                            <label className="text-end fs-5" htmlFor="shopNo">Shop Number </label>
+
+                            <input
+                                type="text"
+                                name='shopNo'
+                                className='form-control'
+                                value={state.shopNo}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required />
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col"></div>
-                        <div className="col"></div>
+
+                    <div className="row mb-2">
+                        <div className="col">
+                            <label className="text-end fs-5" htmlFor="pincode">Pincode </label>
+
+                            <input
+                                type="number"
+                                name='pincode'
+                                className='form-control'
+                                value={state.pincode}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required />
+                        </div>
+
+                        <div className="col">
+                            <label className="text-end fs-5" htmlFor="city">City</label>
+
+                            <input
+                                type="text"
+                                name='city'
+                                className='form-control'
+                                value={state.city}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required />
+                        </div>
                     </div>
-                    <div className="row">
-                        <div className="col"></div>
-                        <div className="col"></div>
+
+                    <div className="row mb-3">
+                        <div className="col">
+                            <label className="text-end fs-5" htmlFor="state">State</label>
+
+                            <input
+                                type="text"
+                                name='state'
+                                className='form-control'
+                                value={state.state}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required />
+                        </div>
+
+                        <div className="col">
+                            <label className="text-end fs-5" htmlFor="city">Country </label>
+
+                            <input
+                                type="text"
+                                name='country'
+                                className='form-control'
+                                value={state.country}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required />
+                        </div>
                     </div>
+
+
+                    {/* Billing Information */}
+                    <div className="row mb-2">
+                        <h3>Billing Info </h3>
+                        <hr />
+
+                        <div className="col">
+                            <label className="text-end fs-5" htmlFor="cardHoldersName">Card Holders Name </label>
+
+                            <input
+                                type="text"
+                                name='cardHoldersName'
+                                className='form-control'
+                                value={state.cardHoldersName}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required />
+                        </div>
+
+                        <div className="col">
+                            <label
+                                className="text-end fs-5"
+                                htmlFor="cardNumber">Card Number
+                            </label>
+
+                            <input
+                                type="text"
+                                name='cardNumber'
+                                className='form-control'
+                                value={state.cardNumber}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required />
+                        </div>
+                    </div>
+                    
+                    <div className="row mb-2">
+                        <div className="col">
+                            <label className="text-end fs-5" htmlFor="state">State</label>
+
+                            <input
+                                type="text"
+                                name='state'
+                                className='form-control'
+                                value={state.cvv}
+                                onChange={handleChange}
+                                disabled={isDisabled}
+                                required />
+                        </div>
+
+                        <div className="col">
+                            <label className="text-end fs-5" htmlFor="Month">Month</label>
+
+                            <select
+                                className="form-select"
+                                id="month"
+                                onChange={handleChange}
+                                value={state.month}
+                                required>
+                                <option value=""> --Select-- </option>
+                                <option value="January">January</option>
+                                <option value="February">February</option>
+                                <option value="March">March</option>
+                                <option value="April">April</option>
+                                <option value="May">May</option>
+                                <option value="June">June</option>
+                                <option value="July">July</option>
+                                <option value="August">August</option>
+                                <option value="September">September</option>
+                                <option value="October">October</option>
+                                <option value="November">November</option>
+                                <option value="December">December</option>
+                            </select>
+                        </div>
+
+                        <div className="col">
+                            <label className="text-end fs-5" htmlFor="city">Year</label>
+
+                            <select
+                                className="form-select"
+                                id="year"
+                                onChange={handleChange}
+                                value={state.year}
+                                required>
+
+                                <option value=""> --Select-- </option>
+                                {tenYears().map((year) => (
+                                    <option key={year} value={year}>
+                                        {year}
+                                    </option>
+                                ))}
+
+                            </select>
+                        </div>
+                    </div>
+                    
+                    <hr/>
+                    <div className="btn btn-outline-primary btn-lg mb-3 d-grid col-2 mx-auto"> Update </div> 
+                   
                 </div>
-
-                <div className="mb-3">
-
-                </div>
-
             </form>
         </div>
-
     )
 }
-
 
 export default UserUpdate;
